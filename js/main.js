38406df0638b832af6e672e6823415276086ef50
kailana-so <72859994+kailana-so@users.noreply.github.com>
2020-10-31 13:19:15 +1100

    counter boxes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-// SCROLL
 window.addEventListener("scroll", () => {
     let page = this;
     let pageTop = this.scrollY;
-    let pageHeight = this.outerHeight / 1.2;
+    let pageHeight = this.outerHeight / 1;
 
     let frames = document.querySelectorAll(".scrollAnim");
     frames.forEach((frame) => {
@@ -11,14 +10,52 @@ window.addEventListener("scroll", () => {
 
         if (
             pageTop >= frameTop - pageHeight &&
-            pageTop < frameTop + frameHeight / 1.2
+            pageTop < frameTop + frameHeight / 1
         ) {
             frame.classList.add("anim");
+            // document.getElementById('box1').classList.add('fade-in');
+            // document.getElementById('box2').classList.add('open-right');
+            console.log("yup")
 
         } else {
             frame.classList.remove("anim");
-
+            // document.getElementById('box1').classList.remove('fade-in');
+            // document.getElementById('box2').classList.remove('open-right');
 
         }
     });
+});
+
+var a = 0;
+$(window).scroll(function() {
+
+    var oTop = $('#counter-box').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-number');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+                    duration: 5250,
+                    delay: 6000,
+                    easing: 'swing',
+                    step: function() {
+                        //$this.text(Math.ceil(this.countNum));
+                        $this.text(Math.ceil(this.countNum).toLocaleString('en'));
+                    },
+                    complete: function() {
+                        $this.text(Math.ceil(this.countNum).toLocaleString('en'));
+                        //alert('finished');
+                    }
+
+                });
+        });
+        a = 1;
+    }
+
 });
